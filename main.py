4313c3b21c6d4ad2777703bf1d8dda5ae2645c10
Groggie <groggy67@yahoo.com>
2025-01-29 10:19:11 +0000

    Refactored main to use the guardrails output validator

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ import nltk
 import pandas as pd
 
 from nltk.tokenize import TweetTokenizer
-from guardrails import Guard
+from guardrails import Guard, OnFailAction
 from pydantic import BaseModel, Field
 from guardrails.hub import RegexMatch
 
 from components import pipeline, struct_logger
-from validators import input_validator
+from validators import input_validator, output_validator
+from pydantic import BaseModel
 
-TOPIC_REGEX = "^\w+(_\w+)?$"
-COMMENT_REGEX = "\w+"
-SENTIMENT_REGEX = "^(positive|negative|neutral)+$"
+
+class Review(BaseModel):
+    topic: str
+    comment: str
+    sentiment: str
+
 
 MODEL = "llama3.2:latest"
 DATA_FILE_PATH = "./data/small_test.csv"
@@ -24,34 +28,38 @@ RESULTS_FILE_PATH = "./data/results"
 
 logger = struct_logger.create_logger(LOG_FILE_PATH)
 
+template = """  
+Give the following information, answer the question from the information present in this text: {{text}}.
+Question={{query}}?
+"""
+
 query = """
 List the topics contained in the text provided. 
 Each topic should have a maximum of two words separated by an underscore.
-list the topics as JSON objects JSON format with the key 'topic' and value being the topic name. a key 'comments' with value of the text being looked at 
-and a key 'sentiment' wuth a value  of the sentiment of the text. 
-Ignore your own knowledge.
-If you cannot answer the question say "I don't know"
+Your resonse should be in JSON.
+The  JSON format has the key 'topic' and value being the topic name. a key 'comments' with value of the text being looked at 
+and a key 'sentiment' with a value  of the sentiment of the text. 
+If you cannot answer the question say set the topic name to 'na'
 
 AND RESPONSE 
-[
   {
-    "topic": "hotel_location",
-    "comment": "Handy location for the train to the airport and for access around the city.", 
-    "sentiment": "positive" 
+    'topic': 'hotel_location',
+    'comment': 'Handy location for the train to the airport and for access around the city.', 
+    'sentiment': 'positive'
   },
   {
-    "topic": "hotel_staff",
-    "comment": "The bar was always busy with good staff and bar meals were good quality.",
-    "sentiment": "positive" 
+    'topic': 'hotel_staff',
+    'comment': 'The bar was always busy with good staff and bar meals were good quality.',
+    'sentiment': 'positive' 
   },
   {
-    "topic": "hotel_facilities",
-    ""comment": "There was a gym and pool downstairs but I never got that far (!!) although colleagues did say they were good.",
-      sentiment: "negative"}
+    'topic': 'hotel_facilities',
+    'comment': 'There was a gym and pool downstairs but I never got that far (!!) although colleagues did say they were good.',
+    'sentiment': 'negative'}
     }
+
 Ignore your own knowledge.
-If you cannot answer the question say 'NA'
-ONLY RETURN THE JSON REQUESTED AND NOTHING ELSE
+Only return JSON.
 
 """
 
@@ -59,16 +67,15 @@ tokenizer_words = TweetTokenizer()
 p = pipeline.ReviewDatasetPipeline(model=MODEL, template=template)
 results = []
 
-input_guard = Guard().use(input_validator.InputValidator(on_fail="exception"))
+input_guard = Guard().use(
+    input_validator.InputValidator(on_fail=OnFailAction.EXCEPTION)
+)
 
-
-class JSON_RESPONSE(BaseModel):
-    topic: str = Field(validators=[RegexMatch(regex=TOPIC_REGEX)])
-    comment: str = Field(validators=[RegexMatch(regex=COMMENT_REGEX)])
-    sentiment: str = Field(validators=[RegexMatch(regex=SENTIMENT_REGEX)])
-
-
-output_guard = Guard.for_pydantic(JSON_RESPONSE)
+output_guard = Guard().use(
+    output_validator.OutputValidatior(
+        pydantic_model=Review, on_fail=OnFailAction.EXCEPTION
+    )
+)
 
 
 def process_row(values, df_index):
@@ -79,19 +86,14 @@ def process_row(values, df_index):
         try:
             input_guard.validate(sentence, metadata={"index": df_index})
             best_result = p.run(text=sentence, query=query)[0]
-            print(best_result, type(best_result))
-
-            # output_guard.validate(json.loads(str(best_result)))
-            # write_to_file(best_result)
+            output_guard.validate(str(best_result))
+            write_to_file(best_result)
         except Exception as e:
             print(e)
             logger.error(f"Error:{e}", status="failed validation", id=id, text=text)
 
 
-def write_to_file(
-    results: List[JSON_RESPONSE], file_path=RESULTS_FILE_PATH, file_type="jsonl"
-):
-    print(f"write to file {results}")
+def write_to_file(results: List[dict], file_path=RESULTS_FILE_PATH, file_type="jsonl"):
     if len(results) > 0:
         with open(f"{file_path}_{MODEL}.{file_type}", "a") as file:
             file.write(results + "\n")
