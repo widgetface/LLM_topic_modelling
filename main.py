04e8dbf8f1d36c48778565178012c907b1dcd15e
Groggie <groggy67@yahoo.com>
2025-01-29 10:47:37 +0000

    Refactored code to merge metadata into the final result

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ class Review(BaseModel):
     topic: str
     comment: str
     sentiment: str
+    id: int
+    post_date: str
 
 
 MODEL = "llama3.2:latest"
@@ -80,23 +82,26 @@ output_guard = Guard().use(
 
 def process_row(values, df_index):
     [text, id, post_date] = values
+    output_metadata = {"id": id, "post_date": post_date}
+
     sentences = [tokenizer_words.tokenize(t) for t in nltk.sent_tokenize(text)]
     for sentence in sentences:
         sentence = " ".join(sentence)
         try:
             input_guard.validate(sentence, metadata={"index": df_index})
             best_result = p.run(text=sentence, query=query)[0]
-            output_guard.validate(str(best_result))
-            write_to_file(best_result)
+            output_guard.validate(str(best_result), metadata=output_metadata)
+            result = json.loads(best_result)
+            if type(result) == dict:
+                result.update(output_metadata)
+                write_to_file(str(result))
         except Exception as e:
-            print(e)
             logger.error(f"Error:{e}", status="failed validation", id=id, text=text)
 
 
-def write_to_file(results: List[dict], file_path=RESULTS_FILE_PATH, file_type="jsonl"):
-    if len(results) > 0:
-        with open(f"{file_path}_{MODEL}.{file_type}", "a") as file:
-            file.write(results + "\n")
+def write_to_file(result: dict, file_path=RESULTS_FILE_PATH, file_type="jsonl"):
+    with open(f"{file_path}_{MODEL}.{file_type}", "a") as file:
+        file.write(result + "\n")
 
 
 df = pd.read_csv(DATA_FILE_PATH)[["text", "hotel_id", "post_date"]]
