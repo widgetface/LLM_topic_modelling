4dea81c745c5a139ba7fb733ddf91bebba323f21
Groggie <groggy67@yahoo.com>
2025-01-31 15:29:11 +0000

    Filter out one ord sentences

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ class Review(BaseModel):
 
 
 MODEL = "llama3.2:latest"
-DATA_FILE_PATH = "./data/small_test.csv"
+DATA_FILE_PATH = "./data/small.csv"
 LOG_FILE_PATH = "./logs/app_logs.jsonl"
 RESULTS_FILE_PATH = "./data/results"
 
@@ -55,18 +55,19 @@ def process_row(values: np.ndarray, df_index: pd.RangeIndex):
     output_metadata = {"id": id, "post_date": post_date}
 
     sentences = [tokenizer_words.tokenize(t) for t in nltk.sent_tokenize(text)]
-    for sentence in sentences:
-        sentence = " ".join(sentence)
-        try:
-            input_guard.validate(sentence, metadata={"index": df_index})
-            best_result = p.run(text=sentence, query=query)[0]
-            output_guard.validate(str(best_result), metadata=output_metadata)
-            result = json.loads(best_result)
-            if type(result) == dict:
-                result.update(output_metadata)
-                write_to_file(str(result))
-        except Exception as e:
-            logger.error(f"Error:{e}", status="failed validation", id=id, text=text)
+    if len(sentences) > 1:
+        for sentence in sentences:
+            sentence = " ".join(sentence)
+            try:
+                input_guard.validate(sentence, metadata={"index": df_index})
+                best_result = p.run(text=sentence, query=query)[0]
+                output_guard.validate(str(best_result), metadata=output_metadata)
+                result = json.loads(best_result)
+                if type(result) == dict:
+                    result.update(output_metadata)
+                    write_to_file(str(result))
+            except Exception as e:
+                logger.error(f"Error:{e}", status="failed validation", id=id, text=text)
 
 
 def write_to_file(
@@ -77,7 +78,9 @@ def write_to_file(
 
 
 df = pd.read_csv(DATA_FILE_PATH)[["text", "hotel_id", "post_date"]]
-df.apply(
-    lambda row: process_row(values=row.values, df_index=df.index),
-    axis=1,
-)
+df[
+    df.apply(
+        lambda row: process_row(values=row.values, df_index=df.index),
+        axis=1,
+    ),
+]
